4c2067f710b2d3d3d69ce3cb7773df0d467b5e7f
victorHL99 <victor.lessa@estudante.ufla.br>
2022-07-07 21:25:17 -0300

    feat: first commit and database file configured

---
+++ src/config/database.ts
@@ -0,0 +1,26 @@
+import pg from 'pg';
+import './setup';
+
+const { Pool } = pg;
+
+const devConfig = {
+  host: "localhost",
+  port: 5432,
+  user: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+}
+
+const prodConfig = {
+  connectionString: process.env.DATABASE_URL,
+}
+
+if(process.env.MODE === "PROD"){
+  prodConfig.ssl = {
+    rejectUnauthorized: false,
+  }
+}
+
+const database = new Pool(process.env.MODE === "PROD" ? prodConfig : devConfig);
+
+export default database;
